33e25908b6e393e9b51b8f19946afe76b3f0ff5a
skyflower <xusiyuan10222@126.com>
2018-04-26 21:56:32 +0800

    解析配置文件，服务器ip，获取本地Ip,添加日志类
    Former-commit-id: 470382ebf1d78b3ccf68d98b481beb79183010eb

--- CarSeat_Recognization/CarSeat_Recognization/ParamManager.h
+++ CarSeat_Recognization/CarSeat_Recognization/ParamManager.h
@@ -15,6 +15,11 @@ private:
 	static CParamManager *m_pInstance;
 
 	void Init();
+	bool parseVector(const char *content, const char*name, std::vector<std::wstring> *pVector);
+	bool parseLineSegment(const char *name, size_t, std::vector<std::wstring> *pVec);
+
+	int parseServerIp(const char *content, const char *name);
+
 
 	char* WcharToChar(wchar_t* wc);
 	wchar_t* CharToWchar(char* c);

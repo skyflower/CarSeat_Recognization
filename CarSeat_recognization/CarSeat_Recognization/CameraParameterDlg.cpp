280d57e13c1f1966d1004b06e2a0ab599887494b
SiyuanXu <xusiyuan10222@126.com>
2018-07-01 09:08:58 +0800

    添加相机参数设置对话框，遗留拍照bug以及曝光时间测试bug
    Former-commit-id: 0b9c59f7fa26aae69bf659b79909be855c422399

--- CarSeat_recognization/CarSeat_Recognization/CameraParameterDlg.cpp
+++ CarSeat_recognization/CarSeat_Recognization/CameraParameterDlg.cpp
@@ -13,6 +13,10 @@ IMPLEMENT_DYNAMIC(CCameraParameterDlg, CDialogEx)
 
 CCameraParameterDlg::CCameraParameterDlg(CWnd* pParent /*=NULL*/)
 	: CDialogEx(IDD_DIALOG_CAMERA_PARAMETER, pParent)
+	, m_dGainDB(0)
+	, m_uExposureTime(0)
+	, m_fCameraFPS(0)
+	, m_pLineCamera(nullptr)
 {
 
 }
@@ -21,14 +25,76 @@ CCameraParameterDlg::~CCameraParameterDlg()
 {
 }
 
+void CCameraParameterDlg::SetLineCamera(CLineCamera * pCamera)
+{
+	if (pCamera == nullptr)
+	{
+		return;
+	}
+	m_pLineCamera = pCamera;
+	m_uExposureTime = m_pLineCamera->GetExposureTime();
+	m_fCameraFPS = m_pLineCamera->GetFrameRate();
+	m_dGainDB = m_pLineCamera->GetGain();
+	
+}
+
 void CCameraParameterDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_ED_DB, m_dGainDB);
+	DDV_MinMaxDouble(pDX, m_dGainDB, 0, 255);
+	DDX_Text(pDX, IDC_ED_EXPOSURE_TIME, m_uExposureTime);
+	DDV_MinMaxUInt(pDX, m_uExposureTime, 0, 2000000);
+	DDX_Text(pDX, IDC_ED_FPS, m_fCameraFPS);
+	DDV_MinMaxFloat(pDX, m_fCameraFPS, 1, 100);
 }
 
 
 BEGIN_MESSAGE_MAP(CCameraParameterDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON_GET_PARAMETER, &CCameraParameterDlg::OnBnClickedButtonGetParameter)
+	ON_BN_CLICKED(IDC_BUTTON_SET_PARAMETER, &CCameraParameterDlg::OnBnClickedButtonSetParameter)
 END_MESSAGE_MAP()
 
 
 // CCameraParameterDlg 消息处理程序
+
+
+void CCameraParameterDlg::OnBnClickedButtonGetParameter()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	if (m_pLineCamera == nullptr)
+	{
+		return;
+	}
+	m_uExposureTime = m_pLineCamera->GetExposureTime();
+	m_fCameraFPS = m_pLineCamera->GetFrameRate();
+	m_dGainDB = m_pLineCamera->GetGain();
+	UpdateData(FALSE); // false 将数值从变量传给控件
+}
+
+
+void CCameraParameterDlg::OnBnClickedButtonSetParameter()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	if (m_pLineCamera == nullptr)
+	{
+		return;
+	}
+	UpdateData(TRUE); // false 将数值从控件传给变量
+	m_pLineCamera->SetExposureTime(m_uExposureTime);
+	m_pLineCamera->SetFrameRate(m_fCameraFPS);
+	m_pLineCamera->SetGain(m_dGainDB);
+}
+
+
+BOOL CCameraParameterDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  在此添加额外的初始化
+	UpdateData(FALSE);
+
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 异常: OCX 属性页应返回 FALSE
+}
